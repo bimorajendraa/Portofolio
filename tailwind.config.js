464128ebd641b75752e36db3f0b9838ddce275fa
bimorajendraa <celapceluporeo@gmail.com>
2025-01-07 12:07:22 +0700

    fix:fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,26 +1,25 @@
 const { nextui } = require("@nextui-org/react");
 
 /** @type {import('tailwindcss').Config} */
-export default {
+module.exports = {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
-    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}"
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
       backgroundImage: {
-        'dots': 'radial-gradient(circle, #B3B3B3 0.1px, transparent 1px)',
+        dots: "radial-gradient(circle, #B3B3B3 0.1px, transparent 1px)",
       },
       backgroundSize: {
-        'dots': '10px 10px',
+        dots: "10px 10px",
       },
       fontFamily: {
-        handlee: ['Handlee', 'sans-serif'],
+        handlee: ["Handlee", "sans-serif"],
       },
     },
   },
   darkMode: "class",
-  plugins: [nextui()]
-}
-
+  plugins: [nextui()],
+};
